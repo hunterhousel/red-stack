5ac8bf4ab433649ea635557658f60845f554af41
hunterhousel <houselhunter415@gmail.com>
2026-03-18 13:24:47 -0600

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-  site: 'https://red-stack.com',
-  integrations: [sitemap()],
-  compressHTML: true,
-});
